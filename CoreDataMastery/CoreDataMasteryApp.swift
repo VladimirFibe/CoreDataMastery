6413ae1b16e711692ba8deec479915e5031ce419
Vladimir Fibe <motiw@icloud.com>
2024-05-05 15:58:29 +0500

    Initial Commit

---
+++ CoreDataMastery/CoreDataMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMasteryApp.swift
+//  CoreDataMastery
+//
+//  Created by MacService on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
